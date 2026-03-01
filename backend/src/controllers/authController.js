c07ea540c29a1d593387cd9d94bd7dafa144597b
Matthew Ramefison <matthewbogosy@gmail.com>
2026-03-01 15:47:31 +0300

    Fix import path for User model

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const Boutique = require('../models/Boutique');
 const Client = require('../models/Client');
 const jwt = require('jsonwebtoken');
